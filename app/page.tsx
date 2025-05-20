bc16bf44fc6db5e4f70f4634720aa651d375ce51
Mehran Shahani <mehranshahani6@gmail.com>
2025-05-20 14:53:46 +0330

    Update: Rearranged sections

--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
       >
         <HeroSection />
         <MottoSection />
-        <ServicesSection />
-        <PortfolioSection />
         <AboutSection />
+        <PortfolioSection />
+        <ServicesSection />
         <TestimonialsSection />
         <ContactSection />
       </main>
