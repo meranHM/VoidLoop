f283cbdcca0f723b5dafa0171e7a9c13c300b7bd
Mehran Shahani <mehranshahani6@gmail.com>
2025-05-21 19:06:02 +0330

    Refine: New Vinyl structure to re-use through project with forwardRef

---
+++ components/ui/Vinyl.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import Image from "next/image"
+import { forwardRef } from "react"
+
+interface VinylProps {
+    className?: string
+}
+
+
+const Vinyl = forwardRef<HTMLDivElement, VinylProps>(({ className }, ref) => {
+    return (
+        <div
+            ref={ref}
+            className={`w-[250px] h-[250px] md:w-[350px] md:h-[350px] lg:w-[400px] lg:h-[400px] xl:w-[500px] xl:h-[500px] z-30 pointer-events-none ${className || ""}`}
+            id="vinyl"
+        >
+            <Image 
+                src="/vinyl.png"
+                alt="Vinyl image"
+                width={720}
+                height={720}
+                className="w-full h-auto"
+            />
+        </div>
+    )
+})
+
+Vinyl.displayName = "Vinyl"
+
+export default Vinyl
+
+
+
