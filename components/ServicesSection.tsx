17afe4e9b78051992578f8ce483a8590a02a8fd7
Mehran Shahani <mehranshahani6@gmail.com>
2025-05-21 14:13:51 +0330

    Update: Real content, text responsiveness, entry animations, service buttons

--- components/ServicesSection.tsx
+++ components/ServicesSection.tsx
@@ -1,31 +1,127 @@
-import SectionContainer from "./SectionContainer"
+"use client"
+
+import SectionContainer from "./ui/SectionContainer"
+import ServiceCard from "./ui/ServiceCard"
+import { useRef, useState } from "react"
+import { disablePageScroll, enablePageScroll } from "@fluejs/noscroll"
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { services } from "@/constants"
+import { Service } from "@/constants/types"
+import ServiceModal from "./modals/ServiceModal"
+
+gsap.registerPlugin(ScrollTrigger)
+
 
 export default function ServicesSection() {
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const [selectedService, setSelectedService] = useState<Service | null>(null)
+
+    const servicesRef = useRef(null)
+
+    useGSAP(() => {
+        const services = servicesRef.current
+
+        // Section animations
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: services,
+                start: "15% center",
+                end: "bottom center",
+                toggleActions: "play none none reverse"
+            }
+        }).from("[data-animate='service-card']", {
+            scale: 0.5,
+            opacity: 0,
+            duration: 0.5,
+            stagger: {
+                each: 0.1,
+                from: "random",
+                ease: "power2.inOut",
+            }
+        })
+
+    }, { scope: servicesRef })
+
+    // Handling opening and closing modals
+    const handleOpenModal = (service: Service) => {
+        if (selectedService || isModalOpen) return
+
+        setSelectedService(service)
+        setIsModalOpen(true)
+        disablePageScroll()
+    }
+
+    const handleCloseModal = () => {
+        if (!selectedService || !isModalOpen) return
+
+        setSelectedService(null)
+        setIsModalOpen(false)
+        enablePageScroll()
+    }
+
+
     return (
         <SectionContainer
             className="bg-rich-black flex-col"
             id="services"
+            ref={servicesRef}
         >
-            <h2
-                className="text-3xl mb-8"
-            >
-                Our Services
-            </h2>
             <div
-                className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-6"
+                className="relative w-full flex justify-start items-center px-10 pt-4 pb-10 md:pt-8 md:pb-16 lg:pt-12 lg:pb-32"
             >
-                <div className="card">
-                    Service 1
-                </div>
-                <div className="card">
-                    Service 2
-                </div>
-                <div className="card">
-                    Service 3
-                </div>
-                <div className="card">
-                    Service 4
-                </div>
+                <article
+                    className="w-full p-6 sm:p-8 md:p-12 lg:p-16 flex flex-col justify-start gap-6"
+                >
+                    <div
+                        className="text-left"
+                    >
+                        <h2
+                            className="uppercase text-3xl sm:text-4xl md:text-5xl lg:text-6xl mb-4"
+                        >
+                            Our Experties
+                        </h2>
+                        <p
+                            className="text-xl sm:text-xl md:text-2xl leading-8"
+                        >
+                            We are a team of dedicated<br />
+                            music producers<br />
+                            with a passion about<br />
+                            sound and how it should feel...
+                        </p>
+                    </div>
+                    <div
+                        className="w-full flex flex-col sm:flex-row items-center justify-center md:space-x-8 pt-12 sm:mt-4 md:mt-8"
+                    >
+                        <p
+                            className="text-muted-gray text-nowrap mb-4 sm:mb-0"
+                        >
+                            Tap to Know More <span className="hidden sm:inline">-&gt;</span> 
+                        </p>
+                        <div
+                            className="grid grid-cols-1 sm:grid-cols-2 sm:grid-rows-2 gap-3 sm:self-end text-center"
+                        >
+                            {services.map((service) => (
+                                <ServiceCard
+                                    key={service.id}
+                                    id={service.id}
+                                    title={service.title}
+                                    dataAnimate="service-card"
+                                    className=""
+                                    handleOpenModal={() => handleOpenModal(service)}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                    {selectedService && (
+                        <ServiceModal 
+                            handleClose={handleCloseModal}
+                            title={selectedService.title}
+                            description={selectedService.description}
+                        />
+                    )}
+                </article>
             </div>
         </SectionContainer>
     )
