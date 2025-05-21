0c558c99907f3f1f9b1b1e0af9a7bd1986b4c8fe
Mehran Shahani <mehranshahani6@gmail.com>
2025-05-21 14:14:25 +0330

    Add: Ids for services

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,6 @@
+import { Service } from "./types"
+
+// Header
 export const navLinks = [
     {
         title: "Home",
@@ -14,5 +17,29 @@ export const navLinks = [
     {
         title: "Contact",
         url: "/contact",
+    },
+]
+
+// Services Section
+export const services: Service[] = [
+    {
+        id: "production",
+        title: "Production",
+        description: "We produce your music from scratch, crafting unique soundscapes tailored to your style.",
+    },
+    {
+        id: "recording",
+        title: "Recording",
+        description: "High-quality recording sessions with professional equipment and sound engineers.",
+    },
+    {
+        id: "mix-mastering",
+        title: "Mix & Mastering",
+        description: "Get your track sounding polished and industry-ready with expert mix & mastering.",
+    },
+    {
+        id:"scoring",
+        title: "Scoring",
+        description: "Custom scores for film, games, and media with cinematic depth and emotion.",
     },
 ]
